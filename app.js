a7a503f42f43bf95c8c79814f57b3263100facb3
Rufus Pollock <rufus.pollock@okfn.org>
2012-03-13 00:42:52 +0000

    [#1,gmaps][m]: switch to google maps - fixes #1.
    * Also different colours for each polyline and info windows.

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+var DEFAULT_PALETTE = ["#CA221D", "#C22769", "#3F93E1", "#481B79", "#6AAC32",
+    "#42928F", "#D32645", "#CD531C", "#EDC92D", "#A5B425", "#211D79",
+    "#449256", "#7A2077", "#CA221D", "#E29826", "#44913D", "#2458A3",
+    "#2458A3", "#14388C"];
+
+function setupGmaps() {
+  var myOptions = {
+    center: new google.maps.LatLng(0, 20),
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"),
+      myOptions);
+
+  var url = 'http://thedatahub.org/api/data/f5d81da5-2e55-4302-8ed2-58401d2c139e/_search'
+  var data = {
+    size: 112
+  };
+  var jqxhr = $.ajax({
+    url: url,
+    data: data,
+    dataType: 'jsonp'
+  });
+  jqxhr.done(function(data) {
+    $.each(data.hits.hits, function(idx, item) {
+      var geojson = item._source.Location;
+      var content = $('<div />');
+      content.append($('<h3 />').text(item._source.Route + ' [' + item._source.Country + ']'));
+      content.append($('<p />').text(item._source.Description));
+      var color = DEFAULT_PALETTE[idx % DEFAULT_PALETTE.length];
+
+      addLineToMap(geojson, color, content.html());
+    });
+  });
+
+  function addLineToMap(geojson, color, content) {
+    var googleOptions = {
+      strokeColor: color,
+      strokeWeight: 4,
+      strokeOpacity: 1
+    };
+    var googleVector = new GeoJSON(geojson, googleOptions);
+    googleVector.setMap(map);
+
+    google.maps.event.addListener(googleVector, 'click', function(event) {
+      var infowindow = new google.maps.InfoWindow({
+        content: content,
+        position: event.latLng
+      });
+      infowindow.open(map);
+    });
+  }
+}
+
 function setupLeaflet() {
   var map = new L.Map('map');
 
